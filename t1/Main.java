9b48f3e76ad2b5f017fbcfade1ef7858856beb16
Hugo Costa Machado <hugocm93@gmail.com>
2017-04-01 11:58:48 -0300

    Dummy files

---
+++ t1/Main.java
@@ -0,0 +1,7 @@
+import javax.crypto.*;
+
+public class Main{
+
+	public static void main (String[] args) throws Exception {
+	}
+}
---
+++ t1/MySignature.java
@@ -0,0 +1,22 @@
+public class MySignature {
+
+	private void initSign( byte[] privateKey){
+
+	}
+
+	private void update( byte[] data){
+
+	}
+
+	private byte[] sign(){
+        return null; 
+	 }
+
+	private void initVerity( byte[] publicKey){
+
+	}
+
+	private boolean verify( byte[] signature){
+        return false;
+	}
+}
