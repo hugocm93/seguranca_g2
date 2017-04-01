43f69791ba4464d1bcf0b73a8641d2d577e142b5
Hugo Costa Machado <hugocm93@gmail.com>
2017-04-01 12:59:29 -0300

    Singleton

--- t1/MySignature.java
+++ t1/MySignature.java
@@ -3,8 +3,25 @@ import java.security.PublicKey;
 
 public class MySignature {
 
+    private static MySignature _md5WithRsa = null;
     private PrivateKey _privateKey;
     private PublicKey _publicKey;
+
+    protected MySignature(){}
+
+    public static MySignature getInstance(String method) {
+        switch(method){
+            case "MD5WITHRSA":
+                if(_md5WithRsa == null) {
+                    _md5WithRsa = new MySignature();
+                }
+                return _md5WithRsa;
+
+            default:
+                String message = "Invalid argument for MySignature";
+                throw new IllegalArgumentException(message);
+        }
+    }
 
 	public void initSign(PrivateKey privateKey){
         _privateKey = privateKey;
