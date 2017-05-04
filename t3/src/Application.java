9bb9d6c9586f2b43364fb37711a9d1515e67e550
Hugo Costa Machado <hugocm93@gmail.com>
2017-05-03 22:42:11 -0300

    jar do connection

--- t3/src/Application.java
+++ t3/src/Application.java
@@ -1,3 +1,7 @@
+//import java.sql.SQLException;
+//import java.sql.Statement;
+//
+//import database.MySQLConnection;
 import presenter.LoginPresenter;
 
 public class Application {
@@ -5,6 +9,14 @@ public class Application {
 	public static void main(String[] args) {
 		LoginPresenter loginPresenter = new LoginPresenter();
 		loginPresenter.showWindow();
+		
+//		Statement stmt = null;
+//		try {
+//			stmt = MySQLConnection.getMySQLConnection().createStatement();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	     
+//      System.out.println(MySQLConnection.statusConnection());
 	}
-	
 }
